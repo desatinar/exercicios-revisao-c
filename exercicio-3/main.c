f10c2dc51f3d5b592f458adbdee33c901048ad59
Rodrigo Santos do Nascimento <91297313+desatinar@users.noreply.github.com>
2024-09-19 13:07:13 -0300

    exercicio 3

---
+++ exercicio-3/main.c
@@ -0,0 +1,13 @@
+//Escreva um algoritmo que exiba 20 vezes a mensagem 
+//“Eu gosto de estudar Algoritmos!”. 
+
+#include <stdio.h>
+
+int main()
+{
+    for(int i = 0; i < 20; i++){
+        printf("Eu gosto de estudar Algoritmos!\n");
+    }
+
+    return 0;
+}
