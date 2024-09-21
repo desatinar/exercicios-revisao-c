82fb4b9213c083c68dd6cb2d4f1836e4d110b5db
paulosantos09 <paulo.asilva@aluno.uniaeso.edu.br>
2024-09-21 16:01:10 -0300

    Update main.c

--- exercicio-1/main.c
+++ exercicio-1/main.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+int main(){
+  int totaltempo, horas, minutos, segundos;
+printf("Digite o tempo em segundos: ");
+ scanf("%d", &totaltempo);
+horas = totaltempo / 3600;
+minutos = (totaltempo % 3600) / 60;
+segundos = totaltempo % 60;
+printf("Tempo equivalente: %d horas, %d minutos e %d segundos\n", horas, minutos, segundos);
+}
