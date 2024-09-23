8ba65b3eddcf09489927140064ec3ef91f961fad
paulosantos09 <paulo.asilva@aluno.uniaeso.edu.br>
2024-09-23 20:44:44 -0300

    Update main.c

--- exercicio-105/main.c
+++ exercicio-105/main.c
@@ -1,31 +1,75 @@
-#include <stdio.h>
-
 int main() {
-int matriz[5][5];
-int x, i, j;
-int encontrado = 0;
-int linha, coluna;
-printf("Digite os elementos da matriz 5x5:\n");
-for (i = 0; i < 5; i++) {
-for (j = 0; j < 5; j++) {
-printf("Elemento [%d][%d]: ", i + 1, j + 1);
-scanf("%d", &matriz[i][j]);
-}}
-printf("Digite o valor a ser buscado: ");
-scanf("%d", &x);
-for (i = 0; i < 5; i++) {
-for (j = 0; j < 5; j++) {
-if (matriz[i][j] == x) {
-encontrado = 1; 
-linha = i + 1;
-coluna = j + 1;
-break;
-}}
-if (encontrado) {
-break; }
-if (encontrado) {
-printf("Valor %d encontrado na linha %d e coluna %d.\n", x, linha, coluna);
-} else {
-printf("Valor %d nao encontrado na matriz.\n", x);}
+int x[TAMANHO], y[TAMANHO];
+int soma[TAMANHO], produto[TAMANHO];
+int diferenca[TAMANHO], interseccao[TAMANHO];
+int uniao[TAMANHO * 2];
+printf("Digite os elementos do vetor x:\n");
+for (int i = 0; i < TAMANHO; i++) {
+int valor;
+printf("Elemento %d: ", i + 1);
+scanf("%d", &valor);
+while (existe(x, i, valor)) {
+printf("Elemento repetido. Por favor, digite um valor único: ");
+scanf("%d", &valor);
+}
+x[i] = valor;
+}
+printf("\nDigite os elementos do vetor y:\n");
+for (int i = 0; i < TAMANHO; i++) {
+printf("Elemento %d: ", i + 1);
+scanf("%d", &valor);
+while (existe(y, i, valor)) {
+printf("Elemento repetido. Por favor, digite um valor único: ");
+scanf("%d", &valor);
+}
+y[i] = valor;
+}
+for (int i = 0; i < TAMANHO; i++) {
+soma[i] = x[i] + y[i];
+produto[i] = x[i] * y[i];
+}
+int d = 0;
+for (int i = 0; i < TAMANHO; i++) {
+if (!existe(y, TAMANHO, x[i])) {
+diferenca[d++] = x[i];
+}
+}
+int inter = 0;
+for (int i = 0; i < TAMANHO; i++) {
+if (existe(y, TAMANHO, x[i])) {
+interseccao[inter++] = x[i];
+}
+}
+int u = 0;
+for (int i = 0; i < TAMANHO; i++) {
+uniao[u++] = x[i];
+}
+for (int i = 0; i < TAMANHO; i++) {
+if (!existe(uniao, u, y[i])) {
+uniao[u++] = y[i];
+}
+}
+printf("\nResultados:\n");
+printf("Soma entre x e y: ");
+for (int i = 0; i < TAMANHO; i++) {
+printf("%d ", soma[i]);
+}
+printf("\nProduto entre x e y: ");
+for (int i = 0; i < TAMANHO; i++) {
+printf("%d ", produto[i]);
+}
+printf("\nDiferença entre x e y: ");
+for (int i = 0; i < d; i++) {
+printf("%d ", diferenca[i]);
+}
+printf("\nInterseção entre x e y: ");
+for (int i = 0; i < inter; i++) {
+printf("%d ", interseccao[i]);
+}
+printf("\nUnião entre x e y: ");
+for (int i = 0; i < u; i++) {
+printf("%d ", uniao[i]);
+}
+printf("\n");
 return 0;
 }
